1dfe961a5adeab00dbc048f77cf6a6b5fd8084ef
Romário Moreira <donprj10@outlook.com>
2019-06-19 15:11:02 -0300

    tabelas atualizadas

---
+++ SystemBase/src/ModeloDeDados/Classes/ContasPagar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ModeloDeDados.Classes
+{
+    public class ContasPagar
+    {
+        [Key]
+        public int ContasPagarId { get; set; }
+        [StringLength(20)]
+        public string CNPJ_CPF { get; set; }
+        public DateTime Vencimento { get; set; }
+        public string Descricao { get; set; }
+        [StringLength(30)]
+        public string FormaPagamento { get; set; }
+        [StringLength(20)]
+        public string Contato { get; set; }
+        public decimal Valor { get; set; }
+
+    }
+}
---
+++ SystemBase/src/ModeloDeDados/Classes/ContasReceber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModeloDeDados.Classes
+{
+    public class ContasReceber
+    {
+        [Key]
+        public int ContasReceberId { get; set; }
+        [StringLength(20)]
+        public string CNPJ_CPF { get; set; }
+        public DateTime Vencimento { get; set; }
+        public string Descricao { get; set; }
+        public string FormaPagamento { get; set; }
+        [StringLength(20)]
+        public string Contato { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
