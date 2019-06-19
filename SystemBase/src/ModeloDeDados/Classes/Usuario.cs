322e99e43fe69a0270e879164c07281ab9e6e205
Romário Moreira <donprj10@outlook.com>
2019-06-18 22:34:06 -0300

    Criação da tabela usuario

---
+++ SystemBase/src/ModeloDeDados/Classes/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModeloDeDados.Classes
+{
+    public class Usuario
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        [StringLength(50)]
+        public string Login { get; set; }
+        [StringLength(40)]
+        public string Senha { get; set; }
+        public int nivelAcesso { get; set; }
+    }
+}
