a8a3971cc46fd199f5819c81e62fdf3f10e3ff1a
Gustavo-alves <37491858+Gustavo-alves@users.noreply.github.com>
2019-06-19 17:49:58 -0300

    tela Conta Receber

---
+++ SystemBase/src/SystemBase/views/ContasReceberCadastroView.xaml.cs
@@ -0,0 +1,69 @@
+﻿using ModeloDeDados.Dados;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SystemBase.views
+{
+    /// <summary>
+    /// Lógica interna para ContasReceberCadastroView.xaml
+    /// </summary>
+    public partial class ContasReceberCadastroView : Window
+    {
+        public ContasReceberCadastroView()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnNovoContasReceber_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtVolta_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtRecarregar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DgMostraContasReber_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnExcluirContasReceber_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAlterarContasReceber_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public void PreencherTabela()
+        {
+            using (DBContexto ctx = new DBContexto())
+            {
+                var consulta = ctx.ContasRecber;
+                    .Include(e => e.Estado)
+                    .Include(c => c.Ceps);
+                dgMostraCidade.ItemsSource = consulta.ToList();
+
+            }
+        }
+    }
+}
