98a5b14524d223746cbfba9253d7bf1464442f1b
Gustavo-alves <37491858+Gustavo-alves@users.noreply.github.com>
2019-06-18 22:56:08 -0300

    Tela Usuario

---
+++ SystemBase/src/SystemBase/views/UsuarioCadastroView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SystemBase.views
+{
+    /// <summary>
+    /// Lógica interna para UsuarioCadastroView.xaml
+    /// </summary>
+    public partial class UsuarioCadastroView : Window
+    {
+        public UsuarioCadastroView()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnNovoUsuario_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtVolta_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtRecarregar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DgMostraUsuario_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAlterarUsuario_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
