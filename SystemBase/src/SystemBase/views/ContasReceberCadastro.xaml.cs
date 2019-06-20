328e8b5ba4846eb72b2fd6f29e1b2be39ea3c3cf
Gustavo-alves <37491858+Gustavo-alves@users.noreply.github.com>
2019-06-19 23:11:37 -0300

    tela de conta a pagar

--- SystemBase/src/SystemBase/views/ContasReceberCadastro.xaml.cs
+++ SystemBase/src/SystemBase/views/ContasReceberCadastro.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ModeloDeDados.Classes;
+using ModeloDeDados.Dados;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,69 @@ namespace SystemBase.views
     /// </summary>
     public partial class ContasReceberCadastro : Window
     {
+        private string op = "";
         public ContasReceberCadastro()
         {
             InitializeComponent();
+             op = "";
+        }
+        public ContasReceberCadastro(ContasReceber cr)
+        {
+            InitializeComponent();
+            op = "alterar";
+            tbCodigo.Text = cr.ContasReceberId.ToString();
+            tbCNPJ_CPF.Text = cr.CNPJ_CPF;
+            tbDescricao.Text = cr.Descricao;
+            dpVencimento.Text = cr.Vencimento.ToString();
+            cbFormaPagamento.Text = cr.FormaPagamento;
+            tbContato.Text = cr.Contato;
+            tbValor.Text = cr.Valor.ToString();
+
+        }
+
+        private void BtnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+
+            ContasReceber cr = new ContasReceber();
+            cr.CNPJ_CPF = tbCNPJ_CPF.Text;
+            cr.Descricao = tbDescricao.Text;
+            cr.Vencimento = dpVencimento.SelectedDate.Value;
+            cr.FormaPagamento = cbFormaPagamento.Text;
+            cr.Contato = tbContato.Text;
+            cr.Valor = Convert.ToDecimal(tbValor.Text);
+
+            if (op == "alterar")
+            {
+                using (DBContexto ctx = new DBContexto())
+                {
+                    cr = ctx.ContasReceber.Find(Convert.ToInt32(tbCodigo.Text));
+                    if (cr != null)
+                    {
+                        cr.CNPJ_CPF = tbCNPJ_CPF.Text;
+                        cr.Descricao = tbDescricao.Text;
+                        cr.Vencimento = dpVencimento.SelectedDate.Value;
+                        cr.FormaPagamento = cbFormaPagamento.Text;
+                        cr.Contato = tbContato.Text;
+                        cr.Valor = Convert.ToDecimal(tbValor.Text);
+                        ctx.SaveChanges();
+                    }
+                }
+            }
+            else
+            {
+                using (var ctx = new DBContexto())
+                {
+                    ctx.ContasReceber.Add(cr);
+                    ctx.SaveChanges();
+                }
+            }
+
+            this.Close();
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
--- SystemBase/src/SystemBase/views/ContasReceberCadastroView.xaml.cs
+++ SystemBase/src/SystemBase/views/ContasReceberCadastroView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ModeloDeDados.Dados;
+﻿using ModeloDeDados.Classes;
+using ModeloDeDados.Dados;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,32 +28,49 @@ namespace SystemBase.views
 
         private void BtnNovoContasReceber_Click(object sender, RoutedEventArgs e)
         {
-
+            new ContasReceberCadastro().Show();
         }
 
         private void BtVolta_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Close();
         }
 
         private void BtRecarregar_Click(object sender, RoutedEventArgs e)
         {
-
+            PreencherTabela();
         }
 
-        private void DgMostraContasReber_Loaded(object sender, RoutedEventArgs e)
+        private void DgMostraContasReceber_Loaded(object sender, RoutedEventArgs e)
         {
-
+            PreencherTabela();
         }
 
         private void BtnExcluirContasReceber_Click(object sender, RoutedEventArgs e)
         {
+            if (dgMostraContasReceber.SelectedIndex >= 0)
+            {
+                ContasReceber cr = (ContasReceber)dgMostraContasReceber.Items[dgMostraContasReceber.SelectedIndex];
+                using (DBContexto ctx = new DBContexto())
+                {
+                    cr = ctx.ContasReceber.Find(cr.ContasReceberId);
+                    ctx.ContasReceber.Remove(cr);
+                    ctx.SaveChanges();
+                }
 
+            }
+            PreencherTabela();
         }
 
         private void BtnAlterarContasReceber_Click(object sender, RoutedEventArgs e)
         {
+            ContasReceber cr = new ContasReceber();
+            if (dgMostraContasReceber.SelectedIndex >= 0)
+            {
+                cr = (ContasReceber)dgMostraContasReceber.Items[dgMostraContasReceber.SelectedIndex];
 
+                new ContasReceberCadastro(cr).Show();
+            }
         }
         public void PreencherTabela()
         {
@@ -60,9 +78,11 @@ namespace SystemBase.views
             {
                 var consulta = ctx.ContasReceber;
                    
-                dgMostraContasReber.ItemsSource = consulta.ToList();
+                dgMostraContasReceber.ItemsSource = consulta.ToList();
 
             }
         }
+
+       
     }
 }
